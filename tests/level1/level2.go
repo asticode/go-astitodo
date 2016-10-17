464e9f3cac4329f908d0a61df2722a2945984111
Quentin Renard <contact@asticode.com>
2016-10-17 23:20:15 +0200

    Added assignee and no-skip flags

--- tests/level1/level2.go
+++ tests/level1/level2.go
@@ -3,12 +3,13 @@ package level1
 func MyFirstFunction() {
 	var a = 1
 	a++
+	// TODO(quentin renard) This is a named TODO
 	var c = 3
 	c++
 }
 
-// TODO Rewrite this entirely
-// because it kinda sucks
+// TODO Here is
+// a multi line todo
 func MySecondFunction() {
 	var b = 2
 	b++
